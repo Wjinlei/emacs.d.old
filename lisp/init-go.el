2225f4ff6aa58970e94063f362b75619544e6868
Wjinlei <1976883731@qq.com>
2019-10-17 23:26:33 +0800

    Fix go tab-width 4

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -1,13 +1,11 @@
-;; go
-(with-eval-after-load 'go
+;; golang
 
+;; 设置tab宽度为4个空格
+(add-hook 'go-mode-hook (lambda () (setq tab-width 4)))
+
+(with-eval-after-load 'go
   ;; 配置开启golang语法检查
   (add-hook 'go-mode-hook 'flycheck-mode)
-
-  ;; 设置tab宽度为4个空格
-  (add-hook 'go-mode-hook
-            (lambda ()
-              (setq tab-width 4)))
 )
 
 (provide 'init-go)
