6241b3daa89de57fd6b0a9afcccf0e0bd3a6d137
Wjinlei <1976883731@qq.com>
2019-10-18 15:52:00 +0800

    Modify go tab-width default

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -1,7 +1,7 @@
 ;; golang
 
 ;; 设置tab宽度为4个空格
-(add-hook 'go-mode-hook (lambda () (setq tab-width 4)))
+;; (add-hook 'go-mode-hook (lambda () (setq tab-width 4)))
 
 (with-eval-after-load 'go
   ;; 配置开启golang语法检查
